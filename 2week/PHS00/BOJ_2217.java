ec1cf9c996f814cf3ad44e9c36e59a1535ddd4b9
PHS00 <hyunsoo159487@naver.com>
2023-10-11 19:56:29 +0900

    success : BOJ_2212 센서 문제 해결

--- 2week/PHS00/BOJ_2217.java
+++ 2week/PHS00/BOJ_2217.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
+// 로프
 public class BOJ_2217 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
