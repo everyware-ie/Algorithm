2751e7cba9ce24deb07d77c747ee10b3d2bb7a06
PHS00 <hyunsoo159487@naver.com>
2023-10-11 18:51:12 +0900

    success : BOJ_2217 로프 문제 해결

--- 2week/PHS00/BOJ_2217.java
+++ 2week/PHS00/BOJ_2217.java
@@ -13,7 +13,7 @@ public class BOJ_2217 {
         for(int i = 0; i < N; i++){
             ropes.add(scanner.nextInt());
         }
-        Collections.sort(ropes);    // 내림차순 정렬
+        Collections.sort(ropes);    // 오름차순 정렬
         Collections.reverse(ropes);    // 내림차순 정렬
         // n 개의 로프로 들어올린다
         // 그런데 예를들어 1개의 로프로 들어올리는 경우가 더 좋을 수 있음
@@ -21,17 +21,12 @@ public class BOJ_2217 {
         // 1. 제일 무거운 로프로 잴 수 있는 무게는 로프가 들 수 있는 최대 무게임
         // 2. 그 다음 무거운 로프를 추가해서 2개의 로프로 들 수 있는 최대 무게는 이 과정에서 추가된 로프의 무게 x2 임
         // 3. 또 그다음 무거운 로프를 추가해서 3개의 로프로 들 수 있는 최대 무게는 이 과정에서 추가된 로프의 무게 x3 임
-        int step = 1;
-        int maxWeight = 0;
+        int maxWeight = -1;
         for(int i = 0; i < N; i++){
             int rope = ropes.get(i);
-//            System.out.println(rope);
-            if(maxWeight <= rope * step){
-                maxWeight = rope * step++;
-//                System.out.println(maxWeight);
-            }else{
-                break;
-            }
+            // 끝까지 돌아보는 코드로 변경 후 정답
+            if(maxWeight < rope * (i+1))
+                maxWeight = rope * (i+1);
         }
         System.out.println(maxWeight);
     }
