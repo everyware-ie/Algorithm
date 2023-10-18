9fae2647d0ccb669ecd631b96dd27dc7488c9988
LimSumi <thddkwl5330@naver.com>
2023-10-18 23:17:04 +0900

    PG [86491] 최소직사각형 문제

--- 3week/Soom2/Programmers_1.py
+++ 3week/Soom2/Programmers_1.py
@@ -14,4 +14,4 @@ def solution(sizes):
     return max(height) * max(weight)
 
 
-print(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
+# print(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
