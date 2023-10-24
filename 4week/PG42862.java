ff698b822f6ace7a43ca1f009278d925e3224b6f
JiEung2 <https://github.com/JiEung2>
2023-10-24 13:36:31 +0900

    PG [42862] 체육복

---
+++ 4week/PG42862.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class PG42862 {
+    class Solution {
+        public int solution(int n, int[] lost, int[] reserve) {
+            int answer = n-lost.length;
+            int rCheck[] = new int[reserve.length];
+            int lCheck[] = new int[lost.length];
+            Arrays.sort(lost);
+            Arrays.sort(reserve);
+            int j = 0;
+            for(int l : lost){
+                for(int i=0; i<reserve.length; i++){
+                    if(l == reserve[i]){
+                        rCheck[i] = 1;
+                        lCheck[j] = 1;
+                        answer++;
+                        break;
+                    }
+                }
+                j++;
+            }
+            j = 0;
+            for(int l : lost){
+
+                for(int i=0; i<reserve.length; i++){
+                    if(rCheck[i] == 1 || lCheck[j] == 1) continue;
+                    else if((reserve[i] == l-1) || (reserve[i] == l+1)){
+                        rCheck[i] = 1;
+                        lCheck[j] = 1;
+                        answer++;
+                        break;
+                    }
+                }
+                j++;
+            }
+            return answer;
+        }
+    }
+}
