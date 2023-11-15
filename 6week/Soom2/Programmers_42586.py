c836aed4b0b7fd2371d762fdb307a5e1981b6d29
LimSumi <thddkwl5330@naver.com>
2023-11-16 00:04:37 +0900

    [PG 42586] 기능개발

---
+++ 6week/Soom2/Programmers_42586.py
@@ -0,0 +1,17 @@
+def solution(progresses, speeds):
+    answer = []
+    time = 0
+    cnt = 0
+
+    while len(progresses) > 0:
+        if progresses[0] + time * speeds[0] >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            cnt += 1
+        else:
+            if cnt > 0:
+                answer.append(cnt)
+                cnt = 0
+            time += 1
+    answer.append(cnt)
+    return answer
