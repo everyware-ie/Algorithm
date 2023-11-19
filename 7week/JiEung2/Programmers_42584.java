2f666670ff688b16de608a4ca1724ba4bfafb8e1
JiEung2 <https://github.com/JiEung2>
2023-11-19 16:32:16 +0900

    [42584. 주식가격]

---
+++ 7week/JiEung2/Programmers_42584.java
@@ -0,0 +1,20 @@
+package JiEung2;
+
+public class Programmers_42584 {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        for(int i=0; i<prices.length-1; i++){
+            for(int j= i+1; j<prices.length; j++){
+                if(prices[i] <= prices[j]) answer[i]++;
+                else{
+                    answer[i]++;
+                    break;
+                }
+            }
+        }
+        answer[prices.length-1] = 0;
+
+        return answer;
+    }
+}
