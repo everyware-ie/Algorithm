b07cc7e22a0588f58bc2cede566479b3218882c3
JiEung2 <https://github.com/JiEung2>
2023-11-29 13:51:58 +0900

    [BOJ11728. 배열 합치기]

---
+++ 9week/JiEung2/BOJ11728.java
@@ -0,0 +1,33 @@
+package JiEung2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ11728 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n + m];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for(int i=n; i<n+m; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        for (int num : arr) {
+            sb.append(num+ " ");
+        }
+        System.out.println(sb);
+    }
+}
